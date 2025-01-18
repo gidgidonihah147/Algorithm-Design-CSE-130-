70b59ec13355dd9ea50f369396cb0ddbff2dc723
gidgidonihah147 <gidgidonihah@byui.edu>
2025-01-18 12:19:35 -0700

    W01 changes

--- Week01/Lab01.py
+++ Week01/Lab01.py
@@ -1,33 +1,49 @@
 # 1. Name: 
-#    -your name-
+#    Tristin Parker
 # 2. Assignment Name: 
 #    Lab 01: Python Review
 # 3. Assignment Description:
-#    -describe what this program is meant to do-
+#    Quick review of python meant to test the students basic knowledge of Python and help review if needed
 # 4. What was the hardest part? Be as specific as possible.
-#    -a paragraph or two about how the assignment went for you-
+#    I havent coded in python for a little bit, I've been doing alot of web based coding for other classees. I had to look up some of the differences between them. Once I got going it came back quick though.
 # 5. How long did it take for you to complete the assignment?
-#    -total time in hours including reading the assignment and submitting the program-  
+#    Roughly 0.4 Hours (Excluding setup of github for the course); 0.6 hours with github and folder setup.
 
 import random
 
-# Game introduction.
+# Let the user know the Game introduction.
+print("This is the 'guess a number' game.")
+print("You try to guess a random number in the smallest number of attempts.")
+print("\tIf you guess too high, you will get the prompt- Too high!")
+print("\tIf you guess too low, you will get the prompt- Too low!")
 
-# Prompt the user for how difficult the game will be. Ask for an integer.
+# Let the user choose the high threshold so the game is not the same each time.
+value_max = int(input("Pick a positive integer as the high threshold: "))
 
 # Generate a random number between 1 and the chosen value.
 value_random = random.randint(1, value_max)
 
-# Give the user instructions as to what he or she will be doing.
+# Prompt the user to start the loop for the game
+print(f"Guess a number between 1 and {value_max}.")
 
-# Initialize the sentinal and the array of guesses.
+# Create the empty aray and create guess to allow the user to override the guess count.
+guesses = []
+guess = 0
 
-# Play the guessing game.
-
+# Play the guessing game. Only loop while we dont equal the number allowing to dump out after the correct guess.
+while guess != value_random:
     # Prompt the user for a number.
+    guess = int(input("> "))
 
     # Store the number in an array so it can be displayed later.
+    guesses.append(guess)
 
-    # Make a decision: was the guess too high, too low, or just right.
+    # Was the guess too high, too low, or just right.
+    if guess > value_random:
+        print("\tToo high!")
+    elif guess < value_random:
+        print("\tToo low!")
 
-# Give the user a report: How many guesses and what the guesses where.
+# Print the guesses made, and the total count of guesses made.
+print(f"You were able to find the number in {len(guesses)} guesses.")
+print("The numbers you guessed were:", guesses)
